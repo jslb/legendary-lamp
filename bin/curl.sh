0e83dc88ac1daf4f05569c5124057312539bd698
Jodie Barnsley <j.barnsley9297@student.leedsbeckett.ac.uk>
2020-12-27 00:10:46 +0000

    lyric curl write to tmp + tidy

--- bin/curl.sh
+++ bin/curl.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -v
-set -x
-PATH=~/legendary-lamp
 
 echo "Enter Your Artist:"
 read artistName
@@ -10,18 +7,16 @@ read songName
 echo "Chosen artist and song: $songName by $artistName"
 
 fileName=$(echo ${songName// /_})
-fileName="${songName}.json"
 
-lyric_endpoint="https://private-anon-b06597b77b-lyricsovh.apiary-proxy.com/v1"
+#artistName="Taylor Swift"
 artistName=$(echo ${artistName// /%20})
+#songName="Red"
 songName=$(echo ${songName// /%20})
 
-data="/${artistName}/${songName}"
-url=${lyric_endpoint}${data}
-#echo $url
-#echo $PATH/tmp/$fileName
-#echo curl -X GET $url #> ../tmp/$fileName #$PATH/tmp/$fileName
-curl GET "https://private-anon-b06597b77b-lyricsovh.apiary-proxy.com/v1/lorde/royals"
-
-#curl -X GET https://private-anon-b06597b77b-lyricsovh.apiary-proxy.com/v1/Taylor%20Swift/Red
-
+lyric_endpoint="https://private-anon-b06597b77b-lyricsovh.apiary-proxy.com/v1"
+#data="Taylor%20Swift/Red"
+data="${artistName}/${songName}"
+url="${lyric_endpoint}/${data}"
+echo $url
+#curl GET https://private-anon-b06597b77b-lyricsovh.apiary-proxy.com/v1/Taylor%20Swift/Red
+curl GET $url > ../tmp/$fileName
