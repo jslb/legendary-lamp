9c42396f6a81edb0350447cfbcd3679c3d4ffff3
Jodie Barnsley <j.barnsley9297@student.leedsbeckett.ac.uk>
2020-12-29 22:35:23 +0000

    mb limit to 100 working

--- bin/musicbrainz.sh
+++ bin/musicbrainz.sh
@@ -5,5 +5,7 @@
 mb_endpoint='http://musicbrainz.org/ws/2/recording/?query=artist:'
 #artist='ac\%2Fdc'
 artist='lorde'
-url=${mb_endpoint}${artist}
+#artist='taylor%swift'
+limit='&limit=100'
+url=${mb_endpoint}${artist}${limit}
 curl GET $url -H "Accept: application/json" | jq  '.recordings[].title' > ../tmp/$artist.song 
