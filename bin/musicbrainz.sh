ef9c06dbc282b96d06fbf4624b149c25fbb286fe
Jodie Barnsley <j.barnsley9297@student.leedsbeckett.ac.uk>
2020-12-29 23:47:09 +0000

    artist.songlist features all listed recordings

--- bin/musicbrainz.sh
+++ bin/musicbrainz.sh
@@ -7,5 +7,28 @@ mb_endpoint='http://musicbrainz.org/ws/2/recording/?query=artist:'
 artist='lorde'
 #artist='taylor%swift'
 limit='&limit=100'
-url=${mb_endpoint}${artist}${limit}
-curl GET $url -H "Accept: application/json" | jq  '.recordings[].title' > ../tmp/$artist.song 
+declare -i offset_val=0
+offset="&offset=${offset_val}"
+url=${mb_endpoint}${artist}${limit}${offset}
+
+curl GET $url -H "Accept: application/json" > $artist.data-$offset_val
+
+declare -i count=$(cat $artist.data-$offset_val | jq '.count')
+
+cat $artist.data-$offset_val | jq  '.recordings[].title' > $artist.songlist
+
+if [[ $count > 100 ]];
+then
+	while [[ $count -gt $offset_val ]];
+	do
+		offset_val=$offset_val+100
+		offset="&offset=${offset_val}"
+		url=${mb_endpoint}${artist}${limit}${offset}
+		curl GET $url -H "Accept: application/json" > $artist.data-$offset_val
+		cat $artist.data-$offset_val | jq  '.recordings[].title' >> $artist.songlist
+
+
+		
+	done
+	echo finsihed
+fi
