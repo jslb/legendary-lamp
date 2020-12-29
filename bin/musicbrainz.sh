2b4c5dd8e49ab28eab79420d16d7b7e6460ec2fc
Jodie Barnsley <j.barnsley9297@student.leedsbeckett.ac.uk>
2020-12-29 22:15:01 +0000

    mb output list of recordings to file

--- bin/musicbrainz.sh
+++ bin/musicbrainz.sh
@@ -2,5 +2,8 @@
 
 #https://musicbrainz.org/doc/MusicBrainz_API/Search
 
-url='http://musicbrainz.org/ws/2/recording/?query=artist:ac\%2Fdc'
-curl GET $url 
+mb_endpoint='http://musicbrainz.org/ws/2/recording/?query=artist:'
+#artist='ac\%2Fdc'
+artist='lorde'
+url=${mb_endpoint}${artist}
+curl GET $url -H "Accept: application/json" | jq  '.recordings[].title' > ../tmp/$artist.song 
