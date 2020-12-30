3d11145eded2dc7672db20c26c276a5220592141
Jodie Barnsley <j.barnsley9297@student.leedsbeckett.ac.uk>
2020-12-30 19:29:18 +0000

    print out song title(all returned)

--- bin/musicbrainz.sh
+++ bin/musicbrainz.sh
@@ -5,8 +5,8 @@
 mb_endpoint='http://musicbrainz.org/ws/2/recording/?query=artist:'
 #artist='ac\%2Fdc'
 #artist='lorde'
-artist='taylor%20swift'
-#artist='queen'
+artistName='easy life'
+artist=$(echo ${artistName// /%20})
 limit='&limit=100'
 declare -i offset_val=0
 offset="&offset=${offset_val}"
@@ -17,9 +17,7 @@ curl GET $url -H "Accept: application/json" > ../tmp/$artist.data-$offset_val
 
 declare -i count=$(cat ../tmp/$artist.data-$offset_val | jq '.count')
 
-#cat ../tmp/$artist.data-$offset_val | jq  '.recordings[].title' > ../tmp/$artist.songlist
 cat ../tmp/$artist.data-$offset_val | jq '.recordings[] | {title: .title, artist: ."artist-credit"[].name}' > ../tmp/$artist.songlist
-#'.[] | {message: .commit.message, name: .commit.committer.name}'
 
 if [[ $count > 100 ]];
 then
@@ -34,4 +32,10 @@ then
 		
 	done
 	echo finsihed
+	rm ../tmp/$artist.data-*
 fi
+
+jq -r '.title' ../tmp/$artist.songlist | while read i; do
+    # do stuff with $i
+	echo i: "$i"
+done
